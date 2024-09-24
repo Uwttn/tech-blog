a72964f09025e372c3e485953afa05d825c4ac0a
jm <133266199+Uwttn@users.noreply.github.com>
2024-09-24 11:15:25 -0400

    Fixed the pages so that it can be publish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const nav = document.getElementsByTagName("nav");
 
-// renders navigation bar
+// Renders the navigation bar
 nav[0].innerHTML = `
-<ul>
-<a id="home"><li>Home</li></a>
-<a id="languages"><li>languages</li></a>
-</ul>
+  <ul>
+    <a id="home"><li>Home</li></a>
+    <a id="code"><li>Coding Languages</li></a>
+  </ul>
 `;
 
 const setLinks = () => {
@@ -13,21 +13,29 @@ const setLinks = () => {
 
   let pageMapping;
 
-  if (homePage.includes("index.html")) {
+  // Check if the current page is inside the "assets/pages" folder
+  if (homePage.includes("/assets/pages/")) {
+    // If we are on a page inside the "assets/pages" folder
     pageMapping = {
-      home: "./index.html",
-      languages: "./assets/pages/1-construction/languages.html",
+      home: "../../../index.html", // Link back to homepage from assets/pages
+      code: "./index.html", // Relative path within assets/pages folder
     };
   } else {
+    // If we are on the homepage or another location
     pageMapping = {
-      home: "../../../index.html",
-      languages: "./languages.html",
+      home: "./index.html", // Link back to homepage
+      code: "./assets/pages/code/index.html", // Full path to the code page from the root
     };
   }
+
+  // Update the href for each link element
   for (const id in pageMapping) {
     const linkElement = document.getElementById(id);
-    linkElement.href = pageMapping[id];
+    if (linkElement) {
+      linkElement.href = pageMapping[id];
+    }
   }
 };
 
+// Call the function to set links
 setLinks();
